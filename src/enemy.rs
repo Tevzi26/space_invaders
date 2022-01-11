f4e2406824d3ed564f4bfb316e09b8f6ff871622
Tevzi26 <knez.matevz123@gmail.com>
2022-01-11 11:44:08 +0100

    added enemy

---
+++ src/enemy.rs
@@ -0,0 +1,10 @@
+use crate::{Materials, Speed, WinSize, SCALE, TIME_STEP};
+use bevy::prelude::*;
+
+pub struct EnemyPlugin;
+
+impl Plugin for EnemyPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
-#![allow(unused)]
+//#![allow(unused)]
 mod player;
 
+use enemy::EnemyPlugin;
 use player::PlayerPlugin;
 
 use bevy::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
+const ENEMY_SPRITE: &str = "enemy_a_01.png";
 const TIME_STEP: f32 = 1. / 60.;
 const SCALE: f32 = 0.5;
 
 pub struct Materials {
     player: Handle<ColorMaterial>,
     laser: Handle<ColorMaterial>,
+    enemy: Handle<ColorMaterial>,
 }
 struct WinSize {
+    #[allow(unused)]
     w: f32,
     h: f32,
 }
+struct ActiveEnemies(f32);
 
 struct Player;
 struct PlayerReadyFire(bool);
 struct Laser;
+struct Enemy;
+
 struct Speed(f32);
 impl Default for Speed {
     fn default() -> Self {
@@ -38,8 +45,10 @@ fn main() {
             height: 676.0,
             ..Default::default()
         })
+        .insert_resource(ActiveEnemies(0.))
         .add_plugins(DefaultPlugins)
         .add_plugin(PlayerPlugin)
+        .add_plugin(EnemyPlugin)
         .add_startup_system(setup.system())
         .run();
 }
@@ -52,13 +61,12 @@ fn setup(
 ) {
     let mut window = windows.get_primary_mut().unwrap();
 
-    //Camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    //Create main resources
     commands.insert_resource(Materials {
         player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
         laser: materials.add(asset_server.load(LASER_SPRITE).into()),
+        enemy: materials.add(asset_server.load(ENEMY_SPRITE).into()),
     });
 
     commands.insert_resource(WinSize {
